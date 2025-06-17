620c5660b87af18e814a06690a4622664596dc1b
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-17 22:55:52 +0600

    Mongooes validation

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -6,6 +6,8 @@ const userSchema = new Schema<IUser>({
     type: String,
     required: true,
     trim: true,
+    minlength: 5,
+    maxlength: 10
   },
 
   lastName: {
@@ -14,8 +16,16 @@ const userSchema = new Schema<IUser>({
     trim: true,
   },
 
+  age: {
+    type: Number,
+    required: true,
+    min: [18, 'Must be at least 6, got {VALUE}'],
+    max: 60,
+  },
+
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
   },
@@ -26,9 +36,10 @@ const userSchema = new Schema<IUser>({
 
   role: {
     type: String,
-    enum: ["user", "admin"],
-    default : 'user'
+    uppercase: true,
+    enum: ["USER", "ADMIN", "SUPERADMIN"],
+    default: "USER",
   },
 });
 
-export const User = model('User', userSchema)
+export const User = model("User", userSchema);

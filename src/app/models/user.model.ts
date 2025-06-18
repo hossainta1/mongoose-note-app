bc2ec3ba8a2a2d871c361782c5837983d5c636c3
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-18 20:05:34 +0600

    Email validation using validator

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,13 +1,14 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
+import validator from "validator"
 
 const userSchema = new Schema<IUser>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, "Must be input the first name"],
     trim: true,
     minlength: 5,
-    maxlength: 10
+    maxlength: 10,
   },
 
   lastName: {
@@ -19,15 +20,24 @@ const userSchema = new Schema<IUser>({
   age: {
     type: Number,
     required: true,
-    min: [18, 'Must be at least 6, got {VALUE}'],
+    min: [18, "Must be at least 6, got {VALUE}"],
     max: 60,
   },
 
   email: {
     type: String,
-    unique: true,
+    unique: [true, "Email must be unique"],
     required: true,
     trim: true,
+    //  validate: {
+    //         validator: function (value) {
+    //             return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)
+    //         },
+    //         message: function (props) {
+    //             return `Email ${props.value} is not valid email`
+    //         }
+    //     }
+        validate: [validator.isEmail, "Invalid Email sent {VALUE}"]
   },
   password: {
     type: String,
@@ -37,7 +47,10 @@ const userSchema = new Schema<IUser>({
   role: {
     type: String,
     uppercase: true,
-    enum: ["USER", "ADMIN", "SUPERADMIN"],
+    enum: {
+      values: ["USER", "ADMIN", "SUPERADMIN"],
+      message: "Role is not valid, got {VALUE} role"
+    },
     default: "USER",
   },
 });

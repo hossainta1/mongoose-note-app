8fd90f815b8d96f94a4f8c59656b35b450109a71
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-06-18 20:36:44 +0600

    Implement Zod validation

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,58 +1,87 @@
-import express, { Request, Response } from "express"
-import { User } from "../models/user.model"
+import express, { Request, Response } from "express";
+import { User } from "../models/user.model";
+import { z } from "zod";
 
-export const usersRoutes = express.Router()
+export const usersRoutes = express.Router();
 
-usersRoutes.post('/create-user', async (req: Request, res: Response) => {
+// apply zod validation
 
-    const body = req.body
+const createUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
 
-    const user = await User.create(body)
+usersRoutes.post("/create-user", async (req: Request, res: Response) => {
+  try {
+    const body = await createUserZodSchema.parseAsync(req.body);
 
-    res.status(201).json({
-        success: true,
-        message: "User created successfuly",
-        user
-    })
-})
-usersRoutes.get('/', async (req: Request, res: Response) => {
-    const users = await User.find()
+    console.log(body, "Zod Body")
 
-    res.status(201).json({
-        success: true,
-        message: "All Users retreived successfuly",
-        users
-    })
-})
-usersRoutes.get('/:userId', async (req: Request, res: Response) => {
-    const userId = req.params.userId
-    const user = await User.findById(userId)
+    const user = await User.create(body);
 
     res.status(201).json({
-        success: true,
-        message: "User retrived successfuly",
-        user
-    })
-})
-usersRoutes.delete('/:userId', async (req: Request, res: Response) => {
-    const userId = req.params.userId
-    const user = await User.findByIdAndDelete(userId)
+      success: true,
+      message: "User created successfuly",
+      user : {}
+    });
+  } catch (error: any) {
+    console.log(error);
+     res.status(400).json({
+      success: false,
+      message: error.message,
+      error
+      
+    });
+  }
+});
 
 
-    res.status(201).json({
-        success: true,
-        message: "User Deleted successfuly",
-        user
-    })
-})
-usersRoutes.patch('/:userId', async (req: Request, res: Response) => {
-    const userId = req.params.userId
-    const updatedBody = req.body;
-    const user = await User.findByIdAndUpdate(userId, updatedBody, { new: true, })
 
-    res.status(201).json({
-        success: true,
-        message: "User updated successfuly",
-        user
-    })
-})
+
+
+
+
+usersRoutes.get("/", async (req: Request, res: Response) => {
+  const users = await User.find();
+
+  res.status(201).json({
+    success: true,
+    message: "All Users retreived successfuly",
+    users,
+  });
+});
+usersRoutes.get("/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const user = await User.findById(userId);
+
+  res.status(201).json({
+    success: true,
+    message: "User retrived successfuly",
+    user,
+  });
+});
+usersRoutes.delete("/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const user = await User.findByIdAndDelete(userId);
+
+  res.status(201).json({
+    success: true,
+    message: "User Deleted successfuly",
+    user,
+  });
+});
+usersRoutes.patch("/:userId", async (req: Request, res: Response) => {
+  const userId = req.params.userId;
+  const updatedBody = req.body;
+  const user = await User.findByIdAndUpdate(userId, updatedBody, { new: true });
+
+  res.status(201).json({
+    success: true,
+    message: "User updated successfuly",
+    user,
+  });
+});
